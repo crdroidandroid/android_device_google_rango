3ed974daaaf8ed35115574af7e4a085c46721d52
306bobby-android <143777624+306bobby-android@users.noreply.github.com>
2026-05-21 08:58:39 -0400

    rango: Update security patch level and product props for May SPL

--- device-rango.mk
+++ device-rango.mk
@@ -62,6 +62,10 @@ PRODUCT_PACKAGES += \
 
 TARGET_RECOVERY_DEFAULT_ROTATION := ROTATION_RIGHT
 
+# AICore
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/conf/aicore_sysconfig.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/aicore_sysconfig.xml
+
 # Satellite
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/conf/allowlist_satellite.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_satellite.xml \
